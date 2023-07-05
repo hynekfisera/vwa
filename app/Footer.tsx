d69f63b599d063b6209f7dde158d51ec1b4c27e9
Hynek Fišera <hynek.fisera@seznam.cz>
2023-07-05 14:22:15 +0200

    Change text color

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer>
       <div className="px-12 xl:px-0 mt-6 w-full max-w-screen-sm mx-auto flex flex-col items-center gap-2 bg-transparent py-8">
         <a href="https://www.buymeacoffee.com/hynekfisera" target="_blank" rel="noreferrer noopener" className="h-8">
-          <img className="h-full w-auto" src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=hynekfisera&button_colour=f472b6&font_colour=000000&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00" alt="Buy Me a Coffee" />
+          <img className="h-full w-auto" src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=hynekfisera&button_colour=f472b6&font_colour=f1f5f9&font_family=Inter&outline_colour=000000&coffee_colour=f1f5f9" alt="Buy Me a Coffee" />
         </a>
         <div className="text-center text-gray-700 font-medium text-xs">
           &copy; {new Date().getFullYear()}{" "}
